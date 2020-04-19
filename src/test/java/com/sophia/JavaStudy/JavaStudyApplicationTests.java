0c955d51f2d57856af3030d1401a9b03eb7fadf3
sophia <rmsql11@naver.com>
2020-04-19 13:05:52 +0900

    Initial commit

---
+++ src/test/java/com/sophia/JavaStudy/JavaStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sophia.JavaStudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
